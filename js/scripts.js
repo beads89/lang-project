20cedb8dba1d3f17b1158579fc110394086f6c72
Brandon Eads <brandon181989@hotmail.com>
2021-08-13 10:33:29 -0700

    add values/submit btn for questions in html and let vars for questions  in js.

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,40 @@
-$(document).ready(function() {
+// Business
+function add(answer1, answer2, answer3, answer4, answer5) {
+  return (answer1 + answer2 + answer3 + answer4 + answer5);
+}
 
+
+
+
+$(document).ready(function() {
 // Show intro page, when clicking "next" button, hide this and show questionnaire 
   $("form#intro").submit(function(e) {
     e.preventDefault();
     $("#intro").hide();
-    $("#questions").show();
+    $("#questions").fadeIn();
   })
 // If else statement that combines combination of answers or uses a point system to
 // determine what language will show up.
 // Displys independently from intro.
   $("form#questions").submit(function(e) {
     e.preventDefault();
-    let id1 = $("select#id1").val();
-    let id2 = $("select#id2").val();
-    let id3 = $("select#id3").val();
-    let id4 = $("select#id4").val();
-    let id5 = $("select#id5").val();
+    let answer1 = parseInt($("#answer1").val());
+    let answer2 = parseInt($("#answer2").val());
+    let answer3 = parseInt($("#answer3").val());
+    let answer4 = parseInt($("#answer4").val());
+    let answer5 = parseInt($("#answer5").val());
+    let result = add(answer1, answer2, answer3, answer4, answer5);
+    // Add if else, for values 
 
+    //Hide questions and show results page
+    $("#questions").fadeOut();
+    $("#results").fadeIn();
   });
 // Displays results, has a "try again" button to take user back to questionaire.
 // Maybe add another button that takes them to intro if they want?
   $("form#results").submit(function(e) {
     e.preventDefault();
+    $("#results").fadeOut();
+    $("#questions").fadeIn();
   })
 });
